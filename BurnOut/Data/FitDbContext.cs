4197bfd00f29f7f775227b4e6e972860051cecf0
Rajaraman <Rajaraman@DESKTOP-E20IQQL>
2020-08-12 01:26:12 +0530

    Add project files.

---
+++ BurnOut/Data/FitDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BurnOut.Data
+{
+    class FitDbContext : DbContext
+    {
+        public FitDbContext()
+        {
+
+        }
+
+        // Configuring Database
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Connecting with Database
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=BurnOut;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        // Configuring or Building Tables
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        // Entities => Tables
+    }
+}
